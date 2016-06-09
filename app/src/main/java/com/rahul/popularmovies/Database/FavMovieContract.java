47495948e7f296825751a878dba573e416cf14df
Rahul Kr. Lohra <tgunix@gmail.com>
2016-06-09 08:30:37 +0530

    Adding Content Provider

---
+++ app/src/main/java/com/rahul/popularmovies/Database/FavMovieContract.java
@@ -0,0 +1,46 @@
+package com.rahul.popularmovies.Database;
+
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+/**
+ * Created by Rahul on 6/8/2016.
+ */
+public class FavMovieContract {
+
+    public static String CONTENT_AUTHORITY = "com.rahul.popularmovies";
+
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    public static final String PATH_FAV_MOVIE = "favMovie";
+
+    public static final class FavMovieEntry implements BaseColumns{
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_FAV_MOVIE).build();
+
+
+//        public static final String CONTENT_TYPE =
+//                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_FAV_MOVIE;
+//        public static final String CONTENT_ITEM_TYPE =
+//                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_FAV_MOVIE;
+
+        public static final String TABLE_NAME = "favMovie";
+
+        public static final String COLUMN_MOVIE_ID = "movie_id";
+        public static final String COLUMN_POSTER_PATH = "poster_path";
+        public static final String COLUMN_OVERVIEW = "overview";
+        public static final String COLUMN_RELEASE_DATE = "release_date";
+        public static final String COLUMN_ORIGINAL_TITLE = "original_title";
+        public static final String COLUMN_VOTE_AVERAGE = "VOTE_AVERAGE";
+
+        public static Uri buildWeatherUri(long id) {
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
+
+
+    }
+
+
+}
---
+++ app/src/main/java/com/rahul/popularmovies/Database/FavMovieHelper.java
@@ -0,0 +1,41 @@
+package com.rahul.popularmovies.Database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Rahul on 6/8/2016.
+ */
+public class FavMovieHelper extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+
+    static final String DATABASE_NAME = "movie.db";
+    public FavMovieHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        final String SQL_CREATE_FAV_MOVIE_TABLE = "CREATE TABLE " + FavMovieContract.FavMovieEntry.TABLE_NAME + " (" +
+                FavMovieContract.FavMovieEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                FavMovieContract.FavMovieEntry.COLUMN_MOVIE_ID + " TEXT UNIQUE NOT NULL, " +
+                FavMovieContract.FavMovieEntry.COLUMN_ORIGINAL_TITLE + " TEXT NOT NULL, " +
+                FavMovieContract.FavMovieEntry.COLUMN_OVERVIEW + " REAL NOT NULL, " +
+                FavMovieContract.FavMovieEntry.COLUMN_POSTER_PATH + " REAL NOT NULL " +
+                FavMovieContract.FavMovieEntry.COLUMN_VOTE_AVERAGE + " REAL NOT NULL " +
+
+                " );";
+
+        db.execSQL(SQL_CREATE_FAV_MOVIE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + FavMovieContract.FavMovieEntry.TABLE_NAME);
+        onCreate(db);
+
+
+    }
+}
