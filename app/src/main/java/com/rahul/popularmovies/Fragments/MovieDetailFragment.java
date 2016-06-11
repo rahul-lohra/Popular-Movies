5dbe4fe5f601d2b9a26da62028f3c38255beb7a0
rahul <rahul>
2016-06-11 18:56:32 +0530

    Some updation regarding multipane

--- app/src/main/java/com/rahul/popularmovies/Fragments/MovieDetailFragment.java
+++ app/src/main/java/com/rahul/popularmovies/Fragments/MovieDetailFragment.java
@@ -1,9 +1,29 @@
 package com.rahul.popularmovies.Fragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.rahul.popularmovies.R;
 
 /**
  * Created by rahulkumarlohra on 11/06/16.
  */
 public class MovieDetailFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.activity_movie_detail, container, false);
+
+        return rootView;
+    }
 }
